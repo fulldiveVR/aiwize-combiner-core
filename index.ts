5762d49b4527e068d89b536734243d3bda2669cd
Timofey <84410960+TimmyGray@users.noreply.github.com>
2025-07-23 13:43:54 -0700

    Forgotten export

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { BrowserBackend, useBackend } from "./src/BrowserBackend";
 export { CombinerWebSocketClient } from "./src/CombinerWebSocketClient";
 export { CombinerRestClient } from "./src/CombinerRestClient";
+export { ContextManagerClient } from "./src/ContextManagerClient";
